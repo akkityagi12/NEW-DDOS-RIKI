9f5e40b2538fb025ec23ece3e7f77b6a58f8a806
Kenyon Schowalter <140055141+akkityagi12@users.noreply.github.com>
2024-05-23 08:58:52 +0530

    Update m.py

--- m.py
+++ m.py
@@ -232,7 +232,7 @@ def handle_bgmi(message):
             response = "✅ Usage :- /bgmi <target> <port> <time>"  # Updated command syntax
     else:
         response = """❌ You Are Not Authorized To Use This Command ❌.
-                      🛒 Please Buy From @Shermodder"""
+                      🛒 Please Buy From @team_akki"""
 
     bot.reply_to(message, response)
 
@@ -270,8 +270,8 @@ def show_help(message):
 🤖 To See Admin Commands:
 💥 /admincmd : Shows All Admin Commands.
 
-🚀 Buy From :- @Shermodder
-🚀 Official Channel :- https://t.me/+bDjQmor4X_s3MDE1
+🚀 Buy From :- @team_akki
+🚀 Official Channel :- https://t.me/SacrificePylOfficals
 '''
     for handler in bot.message_handlers:
         if hasattr(handler, 'commands'):
@@ -286,9 +286,9 @@ def show_help(message):
 @bot.message_handler(commands=['start'])
 def welcome_start(message):
     user_name = message.from_user.first_name
-    response = f'''👋🏻Welcome to The Bot, Sher 💖
+    response = f'''👋🏻Welcome to The Bot, Akki 💖
                     🤖Feel Free to Explore. 
-                    ✅Join :- https://t.me/https://t.me/+bDjQmor4X_s3MDE1'''
+                    ✅Join :- https://t.me/SacrificePylOfficals'''
     bot.reply_to(message, response)
 
 @bot.message_handler(commands=['rules'])
